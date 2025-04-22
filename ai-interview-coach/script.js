51dcb0f23fd72b0b29c112ec88cf3fc916490e71
abhishek <abhishekjaswal1122@gmail.com>
2025-04-22 11:16:03 +0530

    added new quesyion

--- ai-interview-coach/script.js
+++ ai-interview-coach/script.js
@@ -30,3 +30,25 @@ function isPalindrom(str) {
   let  naam = "abhishek";
   let reversedString = naam.split("").reverse().join("");
   console.log(reversedString);
+
+  //DSA part 3
+  // recursion means when any sunction cALL ITSELF
+  function factorial(n){
+    if(n===0){
+        return  1;
+
+    }
+    return n* factorial(n-1);
+
+  }
+  console.log(factorial(5));
+
+
+//Fabonacci series
+function fibonacci(n){
+    if(n<=1){
+        return n;
+    }
+    return fibonacci(n-1)+ fibonacci(n-2);
+    }
+console.log(fibonacci(7));
