19b4d32aab595a39b3b80dbcc60a7b63c71ee56c
abhishek <abhishekjaswal1122@gmail.com>
2025-04-22 10:51:09 +0530

    added script file

---
+++ ai-interview-coach/script.js
@@ -0,0 +1,14 @@
+let array = [1,2,3,4,5,6];
+let reversedArray = array.reverse();
+console.log(reversedArray);
+
+//*Find the Maximum eelemeny;
+let arra = [1,5,9,7,5,6,2,4,8,7];
+let max = Math.max(...arra);
+console.log(max);
+
+//remove duplicates
+let aar1 = [1,5,7,4,2,3,6,4];
+let arr1 = [1,2,3,4,5,678,4];
+let unique = [...new Set(arr1,aar1)];
+console.log(unique);
