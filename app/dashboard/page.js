3339a2d518e17aff0c59cb9493e4c8b7ad180131
abhishekjaswal <abhishekjaswal1122@gmail.com>
2025-07-10 11:42:34 +0530

    added designingf

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -9,24 +9,24 @@ export default function Dashboard() {
 
   return (
     <ProtectedRoute>
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100 p-4">
-        <div className="shadow-lg rounded-2xl p-6 md:p-10 bg-white max-w-3xl w-full space-y-6">
-          <h1 className="text-3xl md:text-4xl font-extrabold text-center text-purple-700 mb-4">
-            🚀 AI Interview Portal Dashboard
+      <div className="min-h-screen flex flex-col items-center justify-center  font-[family-name:var(--font-patrick-hand)] ">
+        <div className="shadow-lg rounded-2xl p-6 md:p-10  max-w-3xl w-full ">
+          <h1 className="text-3xl md:text-4xl font-extrabold text-center text-white mb-4">
+             AI Interview Portal Dashboard
           </h1>
-          <p className="text-center text-lg font-semibold text-gray-700">
+          <p className="text-center text-lg font-semibold text-gradient text-gray-500">
             Welcome to your AI Interview Portal!
           </p>
           <div className="flex flex-col md:flex-row gap-6 justify-center mt-8">
             <Link
               href="/upload"
-              className="bg-purple-600 hover:bg-purple-700 text-white px-6 py-3 rounded-full font-bold text-center transition w-full md:w-auto"
+              className="bg-gray-600 hover:bg-gray-700 text-white px-6 py-3 rounded-full font-bold text-center transition w-full md:w-auto"
             >
               Upload Resume & Get Feedback
             </Link>
             <Link
               href="/interview"
-              className="bg-purple-600 hover:bg-purple-700 text-white px-6 py-3 rounded-full font-bold text-center transition w-full md:w-auto"
+              className="bg-gray-600 hover:bg-gray-700 text-white px-6 py-3 rounded-full font-bold text-center transition w-full md:w-auto"
             >
               Start Mock Interview
             </Link>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import LoginPage from "./login/page";
+// import LoginPage from "./login/page";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
     <div className="pb-0 font-[family-name:var(--font-patrick-hand)]">
-     <LoginPage/>
+     {/* <LoginPage/> */}
+     <Dashboard />
     </div>
    
   );
