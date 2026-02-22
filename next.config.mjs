d88116039760113fcea1bf09e412e205609a7b3d
Abhishek Jaswal <abhishekjaswal1122@gmail.com>
2026-02-22 11:41:13 +0530

    fixed: fixed next config.js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+    };
+
+    // Fix for pdfjs-dist canvas binary error
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false,
+      encoding: false,
+    };
+
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
