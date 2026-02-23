e807600f9da4ea64910ab031452474c668abee51
Abhishek Jaswal <abhishekjaswal1122@gmail.com>
2026-02-23 15:05:02 +0530

    fix: next.config.js file

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: false,     // disable turbopack (recommended for your project)
+  },
+};
+
+module.exports = nextConfig;
